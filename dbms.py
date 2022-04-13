d97fe6403e9ee6a97f0d9161c73648e2efdcfa2c
Pamela <pamelakatali@users.noreply.github.com>
2022-04-13 15:35:31 -0400

    update Btree to do indexing for all cols

--- dbms.py
+++ dbms.py
@@ -1,41 +1,83 @@
 from BTrees.OOBTree import OOBTree
 
 class DBMS:
-  def __init__(self, t):
-    self.databases = []
+  def __init__(self):
+    self.databases = OOBTree() #[]
 
   def create_db(self, db_name):
     new_db = DB(db_name)
-    self.databases.append(new_db)
+    self.databases.update({db_name: new_db})
+
+    print('Databases:',list(self.databases.keys()))
+    return new_db
 
 
 class DB:
   def __init__(self, name):
     self.name = name
-    self.tables = []
+    self.tables = OOBTree() #[]
 
   def create_table(self, table_name, columns, col_types):
-    new_table = Table(table_name)
-    self.tables.append(new_table)
+    new_table = Table(table_name, columns, col_types)
+    self.tables.update({table_name: new_table})
+    print('Tables:',list(self.tables.keys()))
+    return new_table
+
+class Row:
+  def __init__(self, values):
+    self.values = values
+
+  def __len__(self):
+    return len(self.values)
+
+
+  def __eq__(self, other):
+    for i in range(len(self.values)):
+      if self.values[i] != other.values[i]:
+        return False
+    return True
 
 
 class Table:
   def __init__(self, name, columns, col_types, primary_key=None):
     self.columns = columns
     self.col_types = col_types
-    self.rows = OOBTree #[] #store as b-tree based on primary key
+    self.rows = OOBTree() #[] #store as b-tree based on primary key
+    
+    self.col_btrees = {}
+    for c in columns:
+      self.col_btrees[c] = OOBTree() #key, lst of pointers
+
+    #print(self.col_btrees)
     #self.primary_key = primary_key
     #self.foreign_key = None
 
   def insert(self, new_row, row_cols):
     #insert into b-tree
-    ind = len(self.t)
-    self.rows.update({ind:new_row})
+    ind = len(self.rows)
+    row_obj = Row(new_row)
+    self.rows.update({ind:row_obj})
 
-  def update(self, curr_row):
-    
+    for i in range(len(new_row)):
+      curr_col = self.columns[i]
+      if self.col_btrees[curr_col].get(new_row[i]) == None:
+        self.col_btrees[curr_col].update({new_row[i]:[row_obj]})
+      else:
+        output = self.col_btrees[curr_col].get(new_row[i])
+        output.append(row_obj)
+        self.col_btrees[curr_col].update({new_row[i]:output})
 
-  def delete(self, curr_row):
-    #self.rows.remove(curr_row)
+    print('Insert:',list(self.rows.keys()), list(self.rows.values()))
+    print(list(self.col_btrees['name'].keys()))
+    print(list(self.col_btrees['name'].values()))
+    print(list(self.col_btrees['grade'].keys()))
+
+#  def where(self, operator, op_l, op_r):
+#    if operator == 'eq':
+#      self.rows.iterkeys
+
+
+  #def update(self, curr_row):
+  #def delete(self, curr_row):
 
 
--- workspace.py
+++ workspace.py
@@ -3,6 +3,7 @@ from BTrees.OOBTree import OOBTree
 
 from dbms import DBMS, DB, Table
 
+'''
 sql = 'SELECT * FROM trips LIMIT 10;'
 
 #res = Token(sql)
@@ -30,4 +31,27 @@ current_db = covid_app
 current_table = current_db.create_table(table_name, columns, col_types)
 #INSERT INTO
 current_table.insert()
+'''
 
+dbms = DBMS(); 
+#CREATE DATABASE covid_app;
+covid_app = dbms.create_db('school_app') 
+
+#USE covid_app;
+current_db = covid_app
+
+#CREATE table -
+table_name = 'school_directory'
+columns = ['name','age','grade']
+col_types = ['int','int','int']
+current_table = current_db.create_table(table_name, columns, col_types)
+
+#INSERT INTO
+current_table.insert(['jack','8','2'], ['name','age','grade'])
+current_table.insert(['jill','10','3'], ['name','age','grade'])
+current_table.insert(['jack','11','4'], ['name','age','grade'])
+
+
+#WHERE
+#print(self.col_btrees)
+#current_table.where('eq','age','10')
